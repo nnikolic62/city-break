b336c4d33fa41585a399b1cd7219c8c3f7a5e1fd
Nemanja <nemanja.nikolic98@hotmail.com>
2025-05-15 11:05:49 +0200

    init

---
+++ src/main/java/com/city_break/city_break/CityBreakApplication.java
@@ -0,0 +1,13 @@
+package com.city_break.city_break;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityBreakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/city_break/city_break/CityBreakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city_break.city_break;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBreakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
